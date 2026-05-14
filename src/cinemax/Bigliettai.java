73df46d3f29c9652da76d9064508c256f01b26da
Cisafrulli <m.corti2006@gmail.com>
2026-05-14 12:16:25 +0200

    Super di Bigliettai e Proiezionisti

--- src/cinemax/Bigliettai.java
+++ src/cinemax/Bigliettai.java
@@ -1,5 +1,9 @@
 package cinemax;
 
-public class Bigliettai extends Utenti{
+import java.util.Date;
 
+public class Bigliettai extends Utenti{
+    public Bigliettai(String nome, String cognome, String username, String password, Date data_di_nascita, String luogoDomicilio, String ruolo) {
+        super(nome, cognome, username, password, data_di_nascita, luogoDomicilio, ruolo);
+    }
 }
--- src/cinemax/Proiezionisti.java
+++ src/cinemax/Proiezionisti.java
@@ -1,4 +1,9 @@
 package cinemax;
 
+import java.util.Date;
+
 public class Proiezionisti extends Utenti {
+    public Proiezionisti(String nome, String cognome, String username, String password, Date data_di_nascita, String luogoDomicilio, String ruolo) {
+        super(nome, cognome, username, password, data_di_nascita, luogoDomicilio, ruolo);
+    }
 }
