95f4be015e7dcaa6a9b2da0bc2ba795442a9a5f2
danie <danieleditrapani44@gmail.com>
2026-05-21 22:59:42 +0200

    aggiunta la funzione di has per la password

--- src/cinemax/Password.java
+++ src/cinemax/Password.java
@@ -1,4 +1,25 @@
 package cinemax;
 
 public class Password {
+    private static final int DIM_ALFA = 31;
+    private static final int DIM_TAB = 1007;
+    private Utenti utente;
+
+    public Password( Utenti utente){
+        this.utente = utente;
+    }
+
+    public int hash() {
+        String v = this.utente.getPassword();
+        int l = v.length();
+        int hasRisultato = 0;
+        for (int h = 0; h < l; h++) {
+            char c = v.charAt(h);
+            hasRisultato= (DIM_ALFA *  hasRisultato + Character.getNumericValue(c)) % DIM_TAB;
+        }
+        if (hasRisultato <0){
+            hasRisultato += DIM_TAB;
+        }
+        return hasRisultato ;
+    }
 }
